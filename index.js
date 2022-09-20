9d46dc4dff2dce0300739746404831b6349ed247
Kate Galati <kategalati@Kates-MacBook-Air.local>
2022-09-20 16:59:54 -0700

    commit tree 6969995ec87c55e3e5008cde92d812d1ec12ebb6

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+main.remove('main');
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Kate is the champion';
+
+document.body.append(newHeader);
